a5a0a255d9f18a3677e28c1d55f53329bbede744
matiasether <matias.ether@protonmail.com>
2023-01-14 15:42:14 -0300

    El menu mobile ya está añadido

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const desktopEmailMenu = document.querySelector('.navbar-email');
 const desktopUserAccountMenu = document.querySelector('.desktop-menu')
 
+const mobileMenu = document.querySelector('.mobile-menu');
+const iconMenuMobile = document.querySelector('.menu');
+
+//Muestra/quita el menu de cuenta de usuario al darle click al email
 const toggleDesktopUserAccountMenu = () => {
     desktopUserAccountMenu.classList.toggle('inactive')
 }
-
 desktopEmailMenu.addEventListener('click', toggleDesktopUserAccountMenu);
+
+
+//Muestra/quita el menu mobile al darle click
+const toggleMenuMobile = () => {
+    mobileMenu.classList.toggle('inactive')
+}
+iconMenuMobile.addEventListener('click', toggleMenuMobile)
