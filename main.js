ed108397c2e4ba898a16439900798f35c19be78b
matiasether <matias.ether@protonmail.com>
2023-01-28 22:37:58 -0300

    Se termino de crear la funcion que muestra los datos del elemento seleccionado en detalle al presionarlo con su descripcion

--- main.js
+++ main.js
@@ -50,10 +50,10 @@ document.addEventListener('click', (event) => {
     closeAllMenus();
 });
 
-const createCard = (productName, productPrice, imgSrc, productIndex) => {
-    const mainCardsContainer = document.querySelector(".cards-container")
+const createCard = (productName, productPrice, imgSrc, productIndex, description) => {
+    const mainCardsContainer = document.querySelector(".cards-container");
 
-    //Contenedor principal
+    //Contenedor principal, el productIndex se obtiene al crear un producto nuevo, el
     const cardContainer = document.createElement("div");
     cardContainer.setAttribute('class', 'product-card');
     cardContainer.setAttribute('id', `product-card-${productIndex}`);
@@ -64,6 +64,7 @@ const createCard = (productName, productPrice, imgSrc, productIndex) => {
     const cardImg = document.createElement('img');
     const getImgSrc = imgSrc => `./img/${imgSrc}`;
     cardImg.src = getImgSrc(imgSrc)
+    cardImg.setAttribute('id', `product-card__image-${productIndex}`);
     cardContainer.append(cardImg)
 
     //Crea el contenedor del nombre e imagen del producto a (class="product-card")
@@ -75,19 +76,27 @@ const createCard = (productName, productPrice, imgSrc, productIndex) => {
     const createDiv = document.createElement("div");
     productInfo.append(createDiv);
 
+    const descriptionContainer = document.createElement('p')
+        descriptionContainer.setAttribute('id', `product-card__description-${productIndex}`)
+        descriptionContainer.innerText = description;
+        descriptionContainer.classList.add('inactive')
+        productInfo.append(descriptionContainer)
+
     //Crea el elemento del PRECIO del producto
     //se obtiene del parametro de la funcion principal (productPrice)
     const productPriceContainer = document.createElement('p');
+    productPriceContainer.setAttribute('id', `product-card__price-${productIndex}`);
     productPriceContainer.innerText = productPrice;
 
     //Crea el elemento del NOMBRE del producto
     //se obtiene del parametro de la funcion principal (productName)
     const productNameContainer = document.createElement('p')
     productNameContainer.innerText = productName
+    productNameContainer.setAttribute('id', `product-card__name-${productIndex}`);
 
     createDiv.append(productPriceContainer, productNameContainer)
 
-    //Crea el boton "Add to Cart" y lo agrega a su nodo padre (class="product-info").
+    //Boton "Add to Cart"
     const figureContainer = document.createElement('figure');
     productInfo.append(figureContainer);
     const btnAddCart = document.createElement('img')
@@ -95,43 +104,64 @@ const createCard = (productName, productPrice, imgSrc, productIndex) => {
     figureContainer.append(btnAddCart)
 } 
 
+//Aqui donde creamos todos los articulos
 const products = [
-    {name: "Globo Terraqueo", price: "$350", img: "globo.jpeg"},
-    {name: "Tablero de Ajedrez", price: "$420", img: "ajedrez.jpeg"},
-    {name: "Conejo de Peluche", price: "$40", img: "peluche.jpeg"},
-    {name: "Telefono Vintage", price: "$65", img: "telefono.jpeg"},
+    {   
+        name: "Globo Terraqueo", 
+        price: "$350", 
+        img: "globo.jpeg", 
+        description: "Explora el mundo en tu hogar o lugar de trabajo. Una herramienta divertida y educativa para todas las edades."
+    },
+    {
+        name: "Tablero de Ajedrez", 
+        price: "$420", 
+        img: "ajedrez.jpeg", 
+        description: "Elegante, duradero y perfecto para jugar y decorar tu espacio. Mejora tus habilidades y desafía a tus oponentes."
+    },
+    {
+        name: "Conejo de Peluche", 
+        price: "$40", img: "peluche.jpeg", 
+        description: "Un conejo de peluche rosado y suave. Ideal para dormir y jugar. Un compañero perfecto para cualquier lugar."
+    },
+    {
+        name: "Telefono Vintage", 
+        price: "$65", 
+        img: "telefono.jpeg", 
+        description: "Una adición única a cualquier hogar. Su diseño retro y color rosa que le da un toque especial a cualquier ambiente."
+    },
 ]
 
+//Esta funcion se encarga de agregarle el indice a los ID de los productos
 products.forEach((product, index) => {
-    createCard(product.name, product.price, product.img, index);
+    createCard(product.name, product.price, product.img, index, product.description);
 });
 
-const selectProduct = event => {
-    const productId = event.target.getAttribute("data-product-id");
-    const product = products[productId];
-    console.log(product);
-    toggleDetailProductCard();
-    showItemSelected(product);
-}
+const productInfoCard = document.querySelector("#productDetailContainer")
 
-const productCards = document.querySelectorAll('.product-card');
-
-productCards.forEach(productCard => {
-    productCard.addEventListener('click', selectProduct);
+const productInfo = document.querySelectorAll(".product-card");
+productInfo.forEach( (element, index)=> {
+  element.addEventListener('click', event => { 
+    console.log(event.target)
+    enableMenu(productInfoCard)
+    showSelectedProduct(index)
+  });
 });
 
-const productDetail = document.querySelector("#product-detail")
 
-const toggleDetailProductCard = () => {
-    enableMenu(productDetail)
-}
-const showItemSelected = product => {
+
+const showSelectedProduct = index => {
     const productDetailImage = document.querySelector("#product-detail-image");
     const productDetailPrice = document.querySelector("#product-detail-price");
     const productDetailName = document.querySelector("#product-detail-name");
-    const productDetailInfo = document.querySelector("#product-detail-info")
-    productDetailImage.src = product.img;
-    productDetailPrice.innerText = product.price;
-    productDetailName.innerText = product.name;
-    productDetailInfo.innerText = product.info;
+    const productDetailInfo = document.querySelector("#product-detail-info");
+
+    const imgSelected = document.querySelector(`#product-card__image-${index}`);
+    const priceSelected = document.querySelector(`#product-card__price-${index}`);
+    const nameSelected = document.querySelector(`#product-card__name-${index}`);
+    const descriptionSelected = document.querySelector(`#product-card__description-${index}`)
+
+    productDetailImage.src = imgSelected.src;
+    productDetailPrice.innerText = priceSelected.innerText;
+    productDetailName.innerText = nameSelected.innerText;
+    productDetailInfo.innerText = descriptionSelected.innerText;
 }
